505a736871817d40fe4c0a01723318ad6c868d8f
Takuya KIHARA <gray@tacck.net>
2019-03-24 20:10:24 +0900

    :bookmark: Step2

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
 for ($num = 1; $num <= 100; $num++) {
+    echo getFizzBuzz($num) . PHP_EOL;
+}
+
+function getFizzBuzz(int $num): string
+{
     if ($num % 3 === 0 && $num % 5 === 0) {
-        echo 'FizzBuzz' . PHP_EOL;
-    } else if ($num % 3 === 0 && $num % 5 !== 0) {
-        echo 'Fizz' . PHP_EOL;
-    } else if ($num % 3 !== 0 && $num % 5 === 0) {
-        echo 'Buzz' . PHP_EOL;
+        return 'FizzBuzz';
+    } elseif ($num % 3 === 0 && $num % 5 !== 0) {
+        return 'Fizz';
+    } elseif ($num % 3 !== 0 && $num % 5 === 0) {
+        return 'Buzz';
     } else {
-        echo "$num" . PHP_EOL;
+        return "$num";
     }
 }
